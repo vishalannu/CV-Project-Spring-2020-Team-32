eab3a68c1261a242b6014f0c8c928451db82be9a
vishalannu <vishal.reddy@research.iiit.ac.in>
2020-02-25 17:43:12 +0530

    dfd_array

--- shot_detection/shot_boundary_detection.py
+++ shot_detection/shot_boundary_detection.py
@@ -1,13 +1,31 @@
 import numpy as np
+import argparse
 import os
 import cv2
 from copy import deepcopy
 import glob
+import skimage.morphology as morph
 
-def video_to_frames(input_video, out_folder):
+
+def compute_dfd(prev, curr, pixel_wise = False):
+	assert prev.shape == curr.shape, 'Input images must be of same dimensions.'
+	norm_order = 1
+	rows, cols = prev.shape[:2]
+	# ||curr - (prev+	optflow)||
+	#Plus divide bby rows*cols
+	
+	diff = np.abs(curr - prev)
+	diff = diff.ravel()
+	diff = np.linalg.norm(diff, ord = norm_order)
+	diff = diff / rows*cols
+	return diff
+	
+
+def get_DFD_array(input_video, out_folder):
 
 	movie_name = input_video.split('/')[-1]	
 	movie_name = movie_name.split('.')[-2]
+	dfd_path = os.path.join(out_folder,movie_name+'_dfd.npy')
 	print("Movie Name:", movie_name)
 
 	inp = cv2.VideoCapture(input_video)
@@ -25,14 +43,20 @@ def video_to_frames(input_video, out_folder):
 	print()
 	while True :
 		print("\r Frames "+str(j-1)+"/"+str(req_frames)+"finished so far.",end="") 
+		previous = deepcopy(current)
 		ret, current = inp.read()
 		if not ret:
 			break
 		cv2.imwrite(os.path.join(frames_path,img_name +str(j).zfill(6)+'.jpg'),current)
 		j=j+1
 
+		diff = compute_dfd(previous,current)
+		DFD_list.append(diff)	
 	inp.release()
 	
+	DFD_list = np.array(DFD_list)
+	np.save(dfd_path,DFD_list)
+	return DFD_list	
 
 
 if __name__ == '__main__' :
@@ -40,4 +64,4 @@ if __name__ == '__main__' :
 	movie_path = input('Movie Path')
 	folder_path = input('Folder Path')
 	
-	video_to_frames(movie_path,folder_path)	
+	DFD_list = get_DFD_array(movie_path,folder_path)	
