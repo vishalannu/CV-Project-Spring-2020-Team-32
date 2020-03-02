4eb2e772a6ffd0dafb85c234280fa7f42c194517
vishalannu <vishal.reddy@research.iiit.ac.in>
2020-03-02 23:49:49 +0530

    shot boundary

--- shot_detection/shot_boundary_detection.py
+++ shot_detection/shot_boundary_detection.py
@@ -6,6 +6,59 @@ from copy import deepcopy
 import glob
 import skimage.morphology as morph
 
+def shots_arr_from_DFD(DFD_list, input_video, out_folder):
+	
+	movie_name = input_video.split('/')[-1]	
+	movie_name = movie_name.split('.')[-2]
+	shots_path = os.path.join(out_folder,movie_name+'_shots_arr.npy')
+	#Load from cache if already exists.
+	if os.path.exists(shots_path) and os.path.isfile(shots_path):
+		shots_arr = np.load(shots_path)
+		return shots_arr
+
+	#Filter the DFD and find thresholded local maxima for shot boundaries.	
+	DFD_list = np.expand_dims(DFD_list,0) #size = 1 x N-1 N=no of frames
+
+	filter_length = 11
+	# line structure element
+	selem = morph.rectangle(1,filter_lengths)
+	# modified top-hat
+	ocdfd = morph.opening(morph.closing(DFD_list, selem), selem)
+	mtdfd = DFD_list - np.minimum(ocdfd, DFD_list)
+
+	diff_threshold = 7
+	proximity = 8
+
+	# make peaks sharper by diffing twice
+	doublediff_mtdfd = np.diff(np.diff(mtdfd))
+	use = np.abs(doublediff_mtdfd.squeeze())
+
+	# find peaks
+	sharpchange = np.where(use > diff_threshold)[0].tolist()
+	changeloc = []
+	k = 0
+	while k < len(sharpchange):
+		flag = k
+		a = sharpchange[k]
+		while (sharpchange[k] - a) < proximity:
+			k += 1
+			if k == len(sharpchange):
+				break
+		changeloc.append(int(round(np.mean(sharpchange[flag:k]))))
+
+	changeloc = [c + 2 for c in changeloc] 
+	# add one to compensate for the double diff
+	# add one because k[DFD_list] is 0 based indexing while it should have been DFD(t) = F(x,y,t)- F(MC(x,y),t-1) so t starts at 1.
+
+	shots_arr = [[0,changeloc[0]-1]]
+	for i in range(len(changeloc)-1):
+		shots_arr.append([changeloc[i],changeloc[i+1]])
+	Num_frames = DFD_list.shape[1]+1
+	shots_arr.append([changeloc[-1],Num_frames-1])
+		
+	shots_arr = np.array(shots_arr)
+	np.save(shots_path, shots_arr)
+	return shots_arr	
 
 def compute_dfd(prev, curr, pixel_wise = False):
 	assert prev.shape == curr.shape, 'Input images must be of same dimensions.'
@@ -64,4 +117,7 @@ if __name__ == '__main__' :
 	movie_path = input('Movie Path')
 	folder_path = input('Folder Path')
 	
+	print('Getting DFD array ... ')
 	DFD_list = get_DFD_array(movie_path,folder_path)	
+	print('Getting Shot Boundaries array ... ')
+	shot_bounds = shots_arr_from_DFD(DFD_list,folder_path, movie_path)	
