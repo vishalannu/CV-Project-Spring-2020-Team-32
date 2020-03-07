34136495d196099b5c833b5a8a38d144d7cab632
harshavardhan-99 <harrshavardhen.g@research.iiit.ac.in>
2020-03-07 16:05:59 +0530

    changed shot_threading file

--- shot_threading/shot_threading.py
+++ shot_threading/shot_threading.py
@@ -6,6 +6,7 @@ import networkx  as nx
 from shot_boundary_detection import shots_arr_from_DFD
 import matplotlib.pyplot as plt
 from itertools import product
+import argparse
 
 def are_images_similar(filename1, filename2):
 	img1 = cv2.imread(filename1)		  # queryImage
@@ -51,10 +52,11 @@ def are_images_similar(filename1, filename2):
 		return True, n_matches
 	return False, n_matches
 
-def compute_shot_similarity_graph(shots_arr):
+def compute_shot_similarity_graph(shots_arr, out_folder, input_video):
 
-	#Add cache load here.
-	sim_path = 'BBT_S1_ep1_sim_graph.npy'
+	movie_name = input_video.split('/')[-1]	
+	movie_name = movie_name.split('.')[-2]
+	sim_path = os.path.join(out_folder,movie_name+'_sim_graph.npy')
 	if os.path.exists(sim_path) and os.path.isfile(sim_path):
 		adj_mat = np.load(sim_path)
 		G = nx.from_numpy_matrix(adj_mat)
@@ -80,11 +82,11 @@ def compute_shot_similarity_graph(shots_arr):
 			first_of_second = shots_arr[r,0]
 
 			#Those are frame numbers, compute filenames from this.
-			f1 = 'bbt_s01e01_'+str(last_of_first).zfill(6)+'.jpg'
-			f1 = os.path.join('../../../bbt_s01e01_excerpt/',f1)
+			f1 = movie_name+'_'+str(last_of_first).zfill(6)+'.jpg'
+			f1 = os.path.join(out_folder, 'frames/'+f1)
 			
-			f2 = 'bbt_s01e01_'+str(first_of_second).zfill(6)+'.jpg'
-			f2 = os.path.join('../../../bbt_s01e01_excerpt/',f2)
+			f2 = movie_name+'_'+str(first_of_second).zfill(6)+'.jpg'
+			f2 = os.path.join(out_folder, 'frames/'+f2)
 
 			#print("f1,f2",f1,f2)			
 			decision, num_matches = are_images_similar(f1,f2)
@@ -98,11 +100,12 @@ def compute_shot_similarity_graph(shots_arr):
 	np.save(sim_path,np.array(G1.todense()))
 	return G
 
-def similarity_to_threads(Sim_Graph):
+def similarity_to_threads(Sim_Graph, out_folder, input_video):
 	
 	#Load from cache if present
-	movie_name = 'BBT_S1_ep1'
-	shots_file = movie_name+'_shot_threading_sa.npy'
+	movie_name = input_video.split('/')[-1]	
+	movie_name = movie_name.split('.')[-2]
+	shots_file = os.path.join(out_folder,movie_name+'_shot_threading_sa.npy')
 	if os.path.exists(shots_file) and os.path.isfile(shots_file):
 		shots_assigned = np.load(shots_file)
 		return shots_assigned
@@ -148,16 +151,28 @@ def compute_shot_threads(out_folder, input_video):
 	print("Getting the shot boundaries ...")
 	shots_arr = shots_arr_from_DFD(out_folder,input_video)
 	print("Computing Shot Similarity Graph ...")
-	G =	compute_shot_similarity_graph(shots_arr)
+	G =	compute_shot_similarity_graph(shots_arr, out_folder, input_video)
 	print("Applying transitivity and finding cliques ...")
-	shots_assigned = similarity_to_threads(G)
+	shots_assigned = similarity_to_threads(G, out_folder, input_video)
 	return shots_assigned
+
+parser = argparse.ArgumentParser(description="Shot Threading")
+
+parser.add_argument('--inp_file', type=str, help='Path of Input video file', required=True)
+parser.add_argument('--out_folder', type=str, help='NAME of folder to store the output files',required=True)
 
 if __name__ == '__main__':
 
-	#Get shots_arr from shot_boundary_detection
-	out_folder = '../../outputs/'
-	#BBT_S1_ep1.avi
-	input_video = '../../inputs/BBT_S1_ep1.avi'
+	args = parser.parse_args()
+	movie_path = args.inp_file
+	folder_path = os.path.join(os.getcwd(),args.out_folder)
+
+	print(folder_path)
+	assert os.path.exists(movie_path), 'No file exists at '+ movie_path+ '.'
+
+	if not os.path.exists(folder_path):
+		os.mkdir(folder_path)
+	assert os.path.isdir(folder_path),folder_path+ ' already exists and is not a directory. Please specify a different name.'
+
 	print("Performing shot threading..")
-	threads_shots_assigned = compute_shot_threads(out_folder, input_video)
+	threads_shots_assigned = compute_shot_threads(folder_path, movie_path)
