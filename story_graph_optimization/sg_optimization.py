102b8fbfb523fb91f909c1cdb4cb9cc03235f594
made-up-names <shreya.reddy@students.iiit.ac.in>
2020-04-17 23:39:34 +0530

    completed optimization

--- story_graph_optimization/sg_optimization.py
+++ story_graph_optimization/sg_optimization.py
@@ -48,3 +48,43 @@ def perturb_sg_indices(indices,lin_cooc, all_presence, col_diff_mat):
 	pSGindices, _, _, _ = SG
 
 	return pSGindices
+
+def search_crossing_candidates(SG):
+	indices, lin_cooc, all_presence, col_diff_mat = SG
+
+	se_presence = all_presence['se']
+	diff_indices = col_diff_mat@indices
+	crossings_at = np.multiply(diff_indices[:,:-1],diff_indices[:,1:]) < 0
+
+	m1 = se_presence@se_presence.T
+	i,j = np.nonzero(m1); #change the find
+	candidates = np.column_stack([i,j,np.zeros([len(i),1])])
+
+	for t in range(crossings_at.shape[1]):
+		m2 = sp.spatial.distance.squareform(crossings_at[:,t])	
+		i,j = np.nonzero(np.triu(m2)) #change this find
+		m3 = np.column_stack([i,j,(t+1)*np.ones([len(i),1])])
+		candidates = np.vstack([candidates, m3])
+	
+	return candidates
+
+def run_swapping_pass(SG, candidates, bestobj):
+	indices, lin_cooc, all_presence, col_diff_mat = SG
+	n = np.prod(indices.shape)
+	for k in range(candidates.shape[0]):
+		pSGindices = indices
+		i, j, t = candidates[k,:3].astype(np.int)
+		#Swap i, j
+		temp = 	indices[i,t]
+		indices[i,t] = indices[j,t]
+		indices[j,t] = temp
+		
+		indices1 = indices.reshape([n,])
+		currobj = loss_function(indices1, lin_cooc, all_presence , col_diff_mat)
+		if currobj >=bestobj:
+			indices = pSGindices		
+		else:
+			bestobj = currobj
+
+	SG = [indices, lin_cooc, all_presence, col_diff_mat]
+	return SG, bestobj
